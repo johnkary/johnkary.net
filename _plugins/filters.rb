e35a9e61c2083c7372f45f1c15eee0edbe67a3c4
John Kary <john@johnkary.net>
2014-01-01 20:19:12 -0600

    Add tags functionality
    - To each post's summary on homepage
    - A page with all tags on them
    - Each post has its tags listed
    
    Based on https://github.com/danieldevries/jekyll-tag-generator

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -11,5 +11,18 @@ module Jekyll
     def time_rfc2822(time)
       time.rfc2822
     end
+
+    # Hacky way to render list of tags as HTML because Jekyll doesn't easily
+    # allow sorting. So this method sorts the tags before rendering them
+    def render_tags_list(tags)
+      sorted_tags = tags.keys.sort_by! { |tag| tag.downcase }
+
+      str = ''
+      sorted_tags.each { |tag|
+        str << '<li>' + tags[tag].size.to_s + ' - <a href="/tag/' + tag + '">' + tag + '</a></li>'
+      }
+
+      str
+    end
   end
 end
